d13d8a775bc6c741e923567279474458858ee9e4
akansha-nec <91887826+akansha-nec@users.noreply.github.com>
2021-10-29 16:34:09 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+    client id =  A3T6AKIAFJKR45SAWS5Z
+    SECRET = hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+    
+  client_id: A3T6AKIAFJKR45SAWS5Z
+  sec: hjshnk5ex5u34565d4654HJKGjhz545d89sjkjak
+  
+  id: AKIAIX4ONRSG6ODEFVJA
+  key: aAUFXbrEuFC8V8c10vk8AVXIt5TjIbKpUZ9IPc/a
+  
+    myid: AKIAIX4ONRSG6ODEFVJA
+  mykey: aAUFXbrEuFC8V8c10vk8AVXIt5TjIbKpUZ9IPc/a
+  
+  AWSKEY: AKIAIX4ONRSG6ODEFVJA
+  AWSPWD: aAUFXbrEuFC8V8c10vk8AVXIt5TjIbKpUZ9IPc/a
+  
+var pg_port=1212;
+var host="gitguardians.com:9082/BLUDB";
+var user="root";
+var _pass="sup3rstr0ngpass1ForGG";
+
+var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
